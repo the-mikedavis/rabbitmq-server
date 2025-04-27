ddc143c785aa2fe3bb7862c87af62462b551f62e
Aaron Seo <aaronseo@amazon.com>
2025-04-26 23:34:36 -0700

    Filter out sockOrAddr from http auth backend's request query
    (cherry picked from commit 844f25d77aa6f8eb9455b55e10028f3ce2dea51f)

--- deps/rabbitmq_auth_backend_http/src/rabbit_auth_backend_http.erl
+++ deps/rabbitmq_auth_backend_http/src/rabbit_auth_backend_http.erl
@@ -76,8 +76,16 @@ is_internal_property(rabbit_auth_backend_http) -> true;
 is_internal_property(rabbit_auth_backend_cache) -> true;
 is_internal_property(_Other) -> false.
 
+is_internal_none_password(password, none) -> true;
+is_internal_none_password(_, _) -> false.
+
+is_sockOrAddr(sockOrAddr) -> true;
+is_sockOrAddr(_) -> false.
+
 extract_other_credentials(AuthProps) ->
-  PublicAuthProps = [{K,V} || {K,V} <-AuthProps, not is_internal_property(K)],
+  PublicAuthProps = [{K,V} || {K,V} <-AuthProps, not is_internal_property(K) and
+                                                  not is_internal_none_password(K, V) and
+                                                  not is_sockOrAddr(K)],
   case PublicAuthProps of
     [] -> resolve_using_persisted_credentials(AuthProps);
     _ -> PublicAuthProps
